05d385c77765bcb3cb32ce830c3079855dc0f7ff
Kevin Kuhn <55213985+kkuhn317@users.noreply.github.com>
2026-09-02 13:24:46 -0400

    Bulk edit workshop author name

--- cloudflare/metadata-overrides.mjs
+++ cloudflare/metadata-overrides.mjs
@@ -1,4 +1,113 @@
-export const metadataOverrides = new Map([]);
+export const metadataOverrides = new Map([
+  [
+    710,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    735,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    746,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    797,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    823,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    827,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    857,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    873,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    917,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    920,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    954,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    999,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    1002,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    1266,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    1291,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    1296,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    1304,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ],
+  [
+    1318,
+    {
+      "AuthorName": "BookwormKevin"
+    }
+  ]
+]);
 
 export function applyMetadataOverrides(item) {
   const override = metadataOverrides.get(Number(item.Id));
